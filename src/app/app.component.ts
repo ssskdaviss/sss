42d33e50a1c1fd56718306a9e341147c4341fb29
giga shaverdashvili <shaverdashvili.giga230@gmail.com>
2023-12-13 13:35:46 +0400

    sad

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sss';
+}
